25869251a5dc6e8e20f9778be9125a7e833c515e
Abhi <s3666399@student.rmit.edu.au>
2018-04-29 16:48:11 +1000

    Login Menu
    Menus after Login

--- JMoss/src/View/Login.java
+++ JMoss/src/View/Login.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class Login {
 
     public static void main(String args[]) throws IOException {
-
+        Login uLogin=new Login();
         LoginController uLoginController=new LoginController();
         String uName=null;
         String password=null;
@@ -29,11 +29,55 @@ public class Login {
             System.out.println("Wrong Credentials.Try Again");
         }
         }while(!uLoginController.checkLogin(uName,password));
+        int cinemaChoice;
         do {
+            System.out.print("\033[H\033[2J");
+            System.out.flush();
+            System.out.println("Welcome to jMOSS- Java based Movie Search System");
+            System.out.println("------------------------------------------------");
+            System.out.println("Welcome to our booking application");
+            System.out.println("Select the cinema to proceed with:");
+            System.out.println("1. St Kilda");
+            System.out.println("2. Fitzroy");
+            System.out.println("3. Melbourne CBD");
+            System.out.println("4. Sunshine");
+            System.out.println("5. Lilydale");
+             cinemaChoice=sc.nextInt();
+            String cinemaName=null;
+            if(cinemaChoice==1)
+                cinemaName= "St Kilda";
+               else if(cinemaChoice==2)
+            cinemaName= "Fitzroy";
+            else if(cinemaChoice==3)
+                cinemaName= "Melbourne CBD";
+            else if(cinemaChoice==4)
+                cinemaName= "Sunshine";
+            else if(cinemaChoice==5)
+                cinemaName= "Lilydale";
+            if(cinemaChoice<1&&cinemaChoice>5)
+            {
+                System.out.println("Please enter the correct choice!");
 
-            System.out.println();
+            }
+            else
+            {
+                uLogin.cinemaChoice(cinemaName)  ;
 
+            }
         }
-        while (true);
+        while (cinemaChoice<1&&cinemaChoice>5);
+    }
+
+    public  void cinemaChoice(String cinemaName)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Welcome to jMOSS- Java based Movie Search System");
+        System.out.println("------------------------------------------------");
+        System.out.println("Welcome to " + cinemaName +" Cinemas");
+        System.out.println("Please select from the below menu:");
+        System.out.println("1. Make a new Booking");
+        System.out.println("2. Delete a booking");
+        int choice=sc.nextInt();
+        
     }
 }
